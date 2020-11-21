5e6264a27f92129d11ed87adb0b50db9b3aa0cae
Rose Peters <rmpeters3@alaska.edu>
2020-11-20 16:04:53 -0900

    Added project 8

---
+++ Project 8/countwords.cpp
@@ -0,0 +1,13 @@
+// countwords.cpp
+// Kelby Hubbard, Rose Peters, Yali Wang
+//
+//
+// There is no associated header file.
+
+#include <iostream> // for std::cout
+#include <fstream> // for std::ifstream
+
+// countwords
+void countwords(std::ofstream f) {
+    
+}
